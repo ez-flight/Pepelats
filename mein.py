153eeb7bfb2d3fba287cf014c5014a8a9422bcbf
YuBabkin <YuBabkin@gmail.com>
2018-01-27 19:15:53 +0500

    В мейне реализованы первые тесты, испраление некоторых ошибок в читалке.

---
+++ mein.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import readtle
+from readtle import *
+
+def main():
+# Моя первая программа на Питоне!!!
+
+    print('Hello!!!');
+
+
+    a = CatalogTLE()
+
+    print(a);
+    
+    a.ReadFullTLE('catalogs/catalog_2016_12_07.txt')
+    
+    print(a.name[0])    
+    print(a.line1[0])
+    print(a.line2[0])
+    
+    print(' * * *     ----------     * * *');
+    print(a.name[1])    
+    print(a.line1[1])
+    print(a.line2[1])
+    
+    print(' * * *     ----------     * * *');
+    print(a.name[2])    
+    print(a.line1[2])
+    print(a.line2[2])
+    
+    print(' * * *    выбор по имени спутника:     * * *');
+    print('Работаем по спутнику ISS:')
+    print(a.GetLine1('ede'))
+    print(a.GetLine2('323'))
+
+
+    print(' * * *    чтение по имени ИСЗ    * * *');
+    print('  ')
+    
+    b = CatalogTLE()
+    b.ReadTLEsat('catalogs/catalog_2016_12_07.txt', 'ISS')
+    
+    
+    
+# a.ReadFullTLE('catalogs/catalog_2016_12_07.txt');
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
--- readtle.py
+++ readtle.py
@@ -4,106 +4,125 @@
 # ЧЕРНОВИК!!!
 # затем для структуры спутников сделать поля ВРЕМЯ и НОМЕРнорад
 
-class CatalogTLE
+#    def __init__(self):
+#    def ReadTLE_sat(self, catalog_file, SatName):
+#    def ReadFullTLE(self, catalog_file):
+#    def GetLine1(self, name):
+#    def GetLine2(self, name):
+#    def GetJD(year, mon, day):
+#    def calcJD(self, name):
+
+# обработать исключение, если задано несуществующее имя ИСЗ!!!!
+
+class CatalogTLE:
 
     def __init__(self):
-    ''' Инициализация, создание пустых полей
-    '''
+        ''' Инициализация, создание пустых полей
+        '''
+        
+        print('readtle: инициализация')
         self.name   = []
         self.line1  = []
         self.line2  = []
         self.date   = []
 
 
-    def ReadTLE_sat(self, catalog_file, SatName)
-    ''' Чтение информации из каталога по конкретному аппарату
-    '''
-        fid = fopen(catalog_file);
-    
+    def ReadTLEsat(self, catalog_file, SatName):
+        ''' Чтение информации из каталога по конкретному аппарату
+        '''
+        f = open(catalog_file, 'r')
+
         # количество найденных ИСЗ в каталоге
         num_sat = 0;
-        
-        while true:
+
+        while 1:
             # бесконечное построковое чтение, 
             # пока не найдём нужный нам спутник
-            tline_name = f.readline(fid);
-           
-            # достигли конца файла, авершение чтения
+            line_name = f.readline();
+
+            # достигли конца файла, завершение чтения
             if (line_name == -1):
-                fclose(fid)
+                close(fid)
                 break
-                
-            if len(line_name) == 0
-                print('***  Неверное задание имени ИСЗ, либо имени файла!']);
+
+            if len(line_name) == 0:
+                print('***  Неверное задание имени ИСЗ, либо имени файла!')
                 return 0
-            
+
             # главное условие на соответствие спутника и чтение двух строк
             if  length(findstr(tline_name, ISZname)):
                 num_sat += 1
-                      
-                self.name.append(line_name(3:-1))            
-                self.line1.append(f.readline(fid))
-                self.line2.append(f.readline(fid))           
-                self.JD = CalcJD(self.line1)
+                
+                f.name.append(line_name[2:-1])
+                l1 = f.readline()   
+                l2 = f.readline()
+                self.line1.append(l1[2:-1])
+                self.line2.append(l2[2:-1])
+                self.JD = self.CalcJD(l1)
+  
  
 
 
-    def ReadFullTLE(self, catalog_file)
-    ''' Полное чтение каталога TLE
-    '''    
-        fid = fopen(catalog_file);
+    def ReadFullTLE(self, catalog_file):
+        ''' Полное чтение каталога TLE
+        '''    
+        f = open('zarya_2018_01_01_15.txt', 'r')
     
-        # количество найденных ИСЗ в каталоге
-        num_sat = 0;
     
-        while true:
-            num_sat =+ 1
-            line_name = f.readline(fid);
-       
-            # достигли конца файла, завершение чтения
-            if (line_name == -1):
-                fclose(fid)
+        print(' ');
+        print('Чтение каталога FULL:');
+        print(f)
+    
+        while 1:
+            line_name = f.readline()
+            if len(line_name) == 0:
+                # достигли коца файла
+                f.close()
                 break
-         
-            self.name.append(line(3:end))            
-            self.line1.append(f.readline(fid))
-            self.line2.append(f.readline(fid))
-            self.JD = CalcJD(self.line1)
- 
- 
-    def getSAT(self, name)
-    ''' По названию аппарата возвращать две строки параметров его орбиты
-    '''    
+       
+            self.name.append(line_name[2:-1])
+            l1 = f.readline()
+            l2 = f.readline()
+            self.line1.append(l1[2:-1])
+            self.line2.append(l2[2:-1])
+
+            self.JD = self.CalcJD(l1)
+  
+  
+    def GetLine1(self, name):
+        ''' По названию аппарата возвращать первую строку параметров его орбиты
+        '''    
         num = name.index(name)
-        line1 = self.line1(num)
-        line2 = self.line2(num)
+        print(num)
+        return self.line1[num]   
 
 
-    def CalcJD(line_str)
-    ''' По данным первой строки вычисляет MJD привязки эфемерид спутника
-    '''
-        year = float(linestr(19:20))
-        if year < 57
+    def GetLine2(self, name):
+        ''' По названию аппарата возвращать вторую строку параметров его орбиты
+        '''    
+        num = name.index(name)
+        return self.line2[num]   
+
+
+    def CalcJD(self, line_str):
+        ''' По данным первой строки вычисляет MJD привязки эфемерид спутника
+        '''
+        year = float(line_str[19:20])
+        if year < 57:
             year =+ 2000
-        else
+        else:
             year =+ 1900
             
-        n_day = float(linestr(21:32))
+        n_day = float(line_str[21:32])
         
-        # юлианская дата на 1 января
-        JD_0 = GetGD(year, 1, 1);
+        # юлианская дата на 1 января текущего года
+        mon = 1
+        day = 1
+        JD_0 = 1721013.5 + 367*year - int( 7 * (year + int( (mon+9)/12)) / 4) + int(275 * mon / 9) + day;
         
         JD = JD_0 + n_day
         
         return JD
 
 
-    def GetJD(year, mon, day)
-    ''' по значению года, месяца и дня определяет Юлианскую дату
-    '''
-        JD = 1721013.5 + 367*year 
-            - int( 7 * (year + int( (mon+9)/12)) / 4) 
-            + int(275 * mon / 9) + day;
-
-        return JD
 
