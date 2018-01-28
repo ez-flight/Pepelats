9f35b4a4d4eebc89fca7ade2ac1134425f13a26b
Ким Виолетта <vkim@rti-mints.ru>
2018-01-29 00:58:11 +0300

    получаем координаты на время привязки

--- quickresult.py
+++ quickresult.py
@@ -1,5 +1,6 @@
 from sgp4.earth_gravity import wgs72
 from sgp4.io import twoline2rv
+from datetime import *
 
 def getSatellites(tlefile):
     '''достает из тле файла имя спутника и создает экземпляр класса
@@ -17,17 +18,23 @@ def getSatellites(tlefile):
         di[name[:-1]] = sat
     return di
 
-def getCoordinates(tlefile, timetuple):
+def getCoordinates(tlefile, timetuple = -1):
     '''по названию тле файла и заданному в timetuple времени экстраполяции
     вычисляет положение и скорости, также выдает время привязки в
     секундах. timetuple надо задавать строго как (yyyy, mm, dd, hh, MM),
-    mm - месяц ,MM  - минуты)'''
+    mm - месяц ,MM  - минуты). Можно не задавать timetuple, тогда по
+    дефолту будyт выдаваться координаты на время привязки'''
     di = getSatellites(tlefile)
     for name in di:
         sat = di[name]
-        tpriv = sat.jdsatepoch + 17531 * 24 * 3600
+        # TODO: проверить, что время привязки я определяю правильно
+        current = sat.jdsatepoch + 17531 * 24 * 3600
+        if timetuple == -1:
+            time = datetime.fromtimestamp(current)
+            timetuple = (time.year, time.month, time.day, time.hour,
+                    time.minute, time.second + time.microsecond / 1e6)
         pos, velo = sat.propagate(*timetuple)
-        di.update({name: (pos, velo, tpriv)})
+        di.update({name: (pos, velo, current)})
     return di
 
 # def getExtroplation(tlefile):
