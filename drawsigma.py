7b635d21e6b5ccfba23f70d1f3064b3df5457f93
YuBabkin <yubabkin@gmail.com>
2018-03-22 13:56:21 +0300

    Сделаны вычисление модных ошибок в орбитальной системе координат

--- drawsigma.py
+++ drawsigma.py
@@ -13,15 +13,15 @@ from sgp4.propagation import sgp4
 
 from readtle import CatalogTLE
 
-from matplotlib.pyplot import plot
+import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid.inset_locator import inset_axes
 # import numpy as np
 
-from pylab import show
+# from pylab import show
 
 
 def OpenFile():
-    '''  что-то там открываем
+    '''  Открытие файла -- каталога TLE. Для тестовых функций.
     '''
     catalog = CatalogTLE()
     catalog.ReadFullTLE('zarya_2018_01_01_15.txt')
@@ -39,40 +39,40 @@ def FullSigma(x1, x2):
     
 def OrbitSigma(x1, v1, x2, v2):
     ''' Вычисление трёх составляющих ошибки 
-        -- радиальной, 
-        -- в-треке
-        -- cross-треке
+        -- radial, 
+        -- in-track
+        -- cross-track
     '''
-    x0 = x2[0]
-    y0 = x2[1]
-    z0 = x2[2]
+    x = x2[0]     # координаты точки, до которой вычисляем расстояние
+    y = x2[1]
+    z = x2[2]
 
-    # вычисление ошибки 1
+    # радиальная ошибка
     A = x1[0]
     B = x1[1]
     C = x1[2]
     D = - A * x1[0] - B * x1[1] - C * x1[2]
-    sigma1 = (A*x0 + B*y0 + C*z0 + D) / sqrt(A**2 + B**2 + C**2)
+    sigma1 = (A*x + B*y + C*z + D) / sqrt(A**2 + B**2 + C**2)
 
-    # вычисление ошибки 2
+    # ошибка по треку (опережение прогноза)
     A = v1[0]
     B = v1[1]
     C = v1[2]        
     D = - A * x1[0] - B * x1[1] - C * x1[2]
-    sigma2 = (A*x0 + B*y0 + C*z0 + D) / sqrt(A**2 + B**2 + C**2)
+    sigma2 = (A*x + B*y + C*z + D) / sqrt(A**2 + B**2 + C**2)
 
-    # вычисление ошибки 3
-    A = + x1[1] * x2[2] - x2[1] * x1[2]
-    B = - x1[0] * x2[2] + x2[0] * x1[2]
-    C = + x1[0] * x2[1] - x2[0] * x1[1]
+    # кросс-трек, над плоскостью прогноза
+    A = + x1[1] * v1[2] - v1[1] * x1[2]
+    B = - x1[0] * v1[2] + v1[0] * x1[2]
+    C = + x1[0] * v1[1] - v1[0] * x1[1]
     D = 0
-    sigma3 = (A*x0 + B*y0 + C*z0 + D) / sqrt(A**2 + B**2 + C**2)
+    sigma3 = (A*x + B*y + C*z + D) / sqrt(A**2 + B**2 + C**2)
  
     return sigma1, sigma2, sigma3
     
 
 def DrawShort(catalog):
-    '''  Создание графиков на коротких отрезках
+    '''  Создание графиков для коротких интервалов
     '''
 
     sig = []
@@ -94,22 +94,24 @@ def DrawShort(catalog):
 
         sig.append(FullSigma(x1, x2))
 
-    plot(sig, 'c--')
-    plot(sig, 'rx')
+    plt.plot(sig, 'c-')
+    plt.plot(sig, 'rx')
 
-    show()
+    plt.xlabel('Epoсh');
+    plt.ylabel(r'$\sigma R$');
+    plt.title(catalog.name[1]);
+    plt.grid(True)
+    plt.show()
 
 
 def DrawLong(catalog, number = 0):
-    ''' Создание графиков "длинных" отрезков
+    ''' Создание графиков "длинных" интервалов
     '''
-
-    sig = []
-
     line1 = catalog.line1[number]
     line2 = catalog.line2[number]
     sat1 = twoline2rv(line1, line2, wgs72)
 
+    sig = []
     for numsat in range(0, len(catalog.line1)):
         line1 = catalog.line1[numsat]
         line2 = catalog.line2[numsat]
@@ -122,14 +124,18 @@ def DrawLong(catalog, number = 0):
 
         sig.append( FullSigma(x1, x2) )
 
-    plot(sig, 'c--')
-    plot(sig, 'rx')
+    plt.plot(sig, 'c-')
+    plt.plot(sig, 'rx')
 
-    show()
+    plt.xlabel('Epoсh');
+    plt.ylabel(r'$\sigma R$');
+    plt.title(catalog.name[1]);
+    plt.grid(True)
+    plt.show()
 
 
-def draw3(catalog, number = 0):
-    '''
+def Draw3(catalog, number = 0):
+    ''' Графики модных ошибок
     '''
     
     sig1 = []
@@ -150,19 +156,25 @@ def draw3(catalog, number = 0):
         x1, v1 = sgp4(sat1, dT)
         x2, v2 = sgp4(sat2, number)
 
-        s1, s2, s3 = orbitSigma(x0, v0, x2, v2)
+        s1, s2, s3 = OrbitSigma(x1, v1, x2, v2)
 
         sig1.append( s1 )
         sig2.append( s2 )
         sig3.append( s3 )
 
-    plot(sig1, 'c--')
-    plot(sig2, 'r--')
-    plot(sig3, 'g--')
+    plt.plot(sig1, 'c-')
+    plt.plot(sig2, 'r-')
+    plt.plot(sig3, 'g-')
 
-    show()
-    
-    
+    plt.plot(sig1, 'kx')
+    plt.plot(sig2, 'kx')
+    plt.plot(sig3, 'kx')
+
+    plt.xlabel('Epoсh');
+    plt.ylabel(r'$\sigma R $');
+    plt.title(catalog.name[1]);
+    plt.grid(True)    
+    plt.show()
 
 
 def main():
@@ -173,7 +185,7 @@ def main():
     print('Ку! 3')
     DrawLong(catalog)
     print('Ku!!!!')
-    draw3(catalog)
+    Draw3(catalog)
     
 
 
