0383febb60fe444265c075493a036183f79ef27c
Ким Виолетта <vkim@rti-mints.ru>
2018-04-06 20:02:21 +0300

    строим короткие и длинные интервалы, все эфемериды

--- view.py
+++ view.py
@@ -1,27 +1,14 @@
+from imp import reload
 from tkinter import ttk
 import tkinter as tk
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
-import matplotlib.pyplot as mpl
 from math import *
 import numpy as np
 
 from readtle import CatalogTLE
-from calcsigma import *
-
-MAINWIDTH=640
-MAINHEIGHT=402
-ITEMWIDTH=80
-ITEMHEIGHT=32
-BASECOLOR="#c0c0c0"
-ITEMCOLOR="#d3d3d3"
-SHADOWCOLOR="#a9a9a9"
-BORDERCOLOR="#808080"
-HIGHLIGHTCOLOR="#5F9EA0"
-TEXTCOLOR="#484848"
-PROJ='Sattelarium 0.3'
+from config import *
+import workpane
+reload(workpane)
+from workpane import *
 
 class MainWindow(tk.Frame):
     '''
@@ -33,7 +20,7 @@ class MainWindow(tk.Frame):
         self._master.title(PROJ)
         self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
         self.addMenu()
-        self._placeWorkarea()
+        self._placeWorkarea(CatalogTLE())
         self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
         self.winfo_toplevel().geometry("")
 
@@ -58,99 +45,11 @@ class MainWindow(tk.Frame):
         self._workArea.setCatalog(self.catalog)
         return self
 
-    def _placeWorkarea(self):
-        self._workArea = Work(self.master)
+    def _placeWorkarea(self, catalogtle):
+        self._workArea = Work(self.master, catalogtle)
         self._workArea.rowconfigure(0, weight=1)
         self._workArea.columnconfigure(0, weight=1)
         return self
-parameters = [0, 1, 2, 3, 4]
-
-class Work(tk.Frame):
-    def __init__(self, master):
-        tk.Frame.__init__(self, master, width=MAINWIDTH,
-                          height=MAINHEIGHT, bg=BASECOLOR, bd=1,
-                          relief=tk.FLAT)
-        self._master = master
-        self.parameter = parameters[0]
-        self.catalog = CatalogTLE()
-        self.fig = Figure(figsize=(6,4), dpi=100)
-        self.ax = self.fig.add_subplot(111)
-        self._placeControlPane()
-        self._placePlots()
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.grid(row=0, column=0, columnspan=3, sticky="wesn")
-        self.winfo_toplevel().geometry("")
-
-    def setCatalog(self, catalog):
-        self.catalog = catalog
-        return self
-
-    def _placeControlPane(self):
-        self._control= tk.Frame(self, width=400, height=320, bd=1,
-                                relief=tk.FLAT, bg=BASECOLOR)
-        self._control.grid(row=0, column=2, sticky="wesn")
-        self._makeButtons()
-        self._makeCombobox()
-        return self
-
-    def _makeButtons(self):
-        self._processButton = tk.Button(self._control)
-        self._processButton["text"] = "Очистить"
-        self._processButton["bg"] = ITEMCOLOR
-        self._processButton["fg"] = TEXTCOLOR
-        self._processButton["command"] = self._clear_plot
-        self._processButton.grid(column=1, row=0, sticky='w')
-        return self
-
-    def _makeCombobox(self):
-        self.combobox = ttk.Combobox(self._control, values=parameters,
-                state='readonly', textvariable=self.parameter)
-        self.combobox.current(0)
-        self.combobox.bind('<<ComboboxSelected>>', self._plot_of_parameter)
-        self.combobox.grid(row=0,column=0, sticky='w')
-        return self
-
-
-    def _placePlots(self):
-        self._plots = tk.Frame(self, width=400, height=320, bd=1,
-                               relief=tk.FLAT, bg=ITEMCOLOR)
-        self._plots.grid(row=0, column=0, columnspan=2, sticky="wesn")
-        self._makeFigure()
-        return self
-
-    def _makeFigure(self):
-        self.fig.patch.set_facecolor(BASECOLOR)
-        self.ax.set_facecolor(SHADOWCOLOR)
-        with mpl.style.context('./presentation.mplstyle'):
-            self.ax.grid(True)
-        self.canvas = FigureCanvasTkAgg(self.fig, self._plots)
-        self.canvas.get_tk_widget().pack(side="top", fill=tk.BOTH, expand=1)
-        self.toolbar = NavigationToolbar2TkAgg(self.canvas, self._plots)
-        self.toolbar["background"] = BASECOLOR
-        self.toolbar.update()
-        self.canvas._tkcanvas.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
-        self.canvas.show()
-        return self
-
-    def _clear_plot(self):
-        with mpl.style.context('./presentation.mplstyle'):
-            self.ax.clear()
-            self.ax.grid(True)
-            self.fig.canvas.draw()
-        return self
-
-    def _plot_of_parameter(self, event):
-        with mpl.style.context('./presentation.mplstyle'):
-            self.ax.plot(CalcShort_ephem(self.catalog, int(self.combobox.get())))
-            self.ax.set_xlabel(r'Epoch')
-            self.ax.set_ylabel(r'%s' % self.combobox.get())
-            self.ax.set_title(r'Короткие интервалы')
-            self.ax.grid(True, color='white', linewidth=2, which='both', alpha=0.3)
-            self.fig.canvas.draw()
-        return self
-
 
 root = tk.Tk()
 app = MainWindow(master=root)
---
+++ workpane.py
@@ -0,0 +1,155 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as mpl
+
+from calcsigma import *
+from config import *
+
+class Work(tk.Frame):
+    def __init__(self, master, catalogtle):
+        tk.Frame.__init__(self, master, width=MAINWIDTH,
+                          height=MAINHEIGHT, bg=BASECOLOR, bd=1,
+                          relief=tk.FLAT)
+        self._master = master
+        self.ephemeris = ephemerisNames[0]
+        self.catalog = catalogtle
+        self.fig = Figure(figsize=(6,4), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self._placeControlPane()
+        self._placePlots()
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.grid(row=0, column=0, columnspan=3, sticky="wesn")
+        self.winfo_toplevel().geometry("")
+
+    def setCatalog(self, catalog):
+        self.catalog = catalog
+        self._scalePeriod['from_'] = 0
+        self._scalePeriod['to'] = len(self.catalog.name) - 1
+        return self
+
+    def _placeControlPane(self):
+        self._control = tk.Frame(self, width=400, height=320, bd=1,
+                                relief=tk.FLAT, bg=BASECOLOR)
+        self._control.grid(row=0, column=2, sticky="wesn")
+        self._makePeriodTypeFrame()
+        self._makeParametersFrame()
+        self._makeScalePeriod()
+        return self
+
+    def _makeButtons(self):
+        self._processButton = tk.Button(self._control)
+        self._processButton["text"] = "Очистить"
+        self._processButton["bg"] = ITEMCOLOR
+        self._processButton["fg"] = TEXTCOLOR
+        self._processButton["command"] = self._clear_plot
+        self._processButton.grid(column=0, row=2, sticky='we')
+        return self
+
+    def _makePeriodTypeFrame(self):
+        self._periodArea = tk.Frame(self._control, width=400, height=20,
+                relief=tk.FLAT)
+        self._periodArea.grid(row=0, column=0, sticky='we')
+        self._periodType = tk.IntVar()
+        self._periodTypeButton = tk.Radiobutton(self._periodArea,
+                bg=BASECOLOR, fg=TEXTCOLOR, indicatoron=1, height=3,
+                activebackground=HIGHLIGHTCOLOR, takefocus="on",
+                text="Короткие интервалы", variable=self._periodType,
+                command=self._isToggledShort, value=0)
+        self._periodTypeButton.grid(row=0, column=0, sticky='we')
+        self._periodTypeButton.select()
+        self._periodTypeButton= tk.Radiobutton(self._periodArea,
+                bg=BASECOLOR, fg=TEXTCOLOR, indicatoron=1, height=3,
+                activebackground=HIGHLIGHTCOLOR,
+                text="Длинные интервалы", variable=self._periodType,
+                command=self._isToggledLong, value=1)
+        self._periodTypeButton.grid(row=0, column=1, sticky='we')
+        return self
+
+    def _makeParametersFrame(self):
+        self.parametersArea = tk.Frame(self._control, width=400,
+                height=50, relief=tk.FLAT, bg=BASECOLOR)
+        self.parametersArea.grid(row=1, column=0, columnspan=2, sticky='we')
+        self._makeEphemerisBox()
+        self._makeButtons()
+        return self
+
+    def _isToggledShort(self):
+        self._clear_plot()
+        return self
+
+    def _isToggledLong(self):
+        self._clear_plot()
+        self._placeScalePeriod()
+        return self
+
+    def _makeEphemerisBox(self):
+        self.ephemerisBox = ttk.Combobox(self.parametersArea, values=ephemerisNames,
+                state='readonly', textvariable=self.ephemeris)
+        self.ephemerisBox.current(0)
+        self.ephemerisBox.configure(width=40, height=80)
+        self.ephemerisBox.bind('<<ComboboxSelected>>', self._plot_of_ephemeris)
+        self.ephemerisBox.grid(row=0,column=0, sticky='we')
+        return self
+
+    def _placeScalePeriod(self):
+        self._scalePeriod.grid(row=1, column=0, sticky='we')
+        return self
+
+    def _makeScalePeriod(self):
+        self._scalePeriod = tk.Scale(self.parametersArea,
+                orient= tk.HORIZONTAL,length=250,from_=0,to=1,tickinterval=10,
+                resolution=1, bg=BASECOLOR)
+        self._scalePeriod.bind('<ButtonRelease-1>', self._plot_of_ephemeris)
+        return self
+
+
+    def _placePlots(self):
+        self._plots = tk.Frame(self, width=400, height=320, bd=1, relief=tk.FLAT, bg=ITEMCOLOR)
+        self._plots.grid(row=0, column=0, columnspan=2, sticky="wesn")
+        self._makeFigure()
+        return self
+
+    def _makeFigure(self):
+        self.fig.patch.set_facecolor(BASECOLOR)
+        self.ax.set_facecolor(SHADOWCOLOR)
+        with mpl.style.context('./presentation.mplstyle'):
+            self.ax.grid(True)
+        self.canvas = FigureCanvasTkAgg(self.fig, self._plots)
+        self.canvas.get_tk_widget().pack(side="top", fill=tk.BOTH, expand=1)
+        self.toolbar = NavigationToolbar2TkAgg(self.canvas, self._plots)
+        self.toolbar["background"] = BASECOLOR
+        self.toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+        self.canvas.show()
+        return self
+
+    def _clear_plot(self):
+        with mpl.style.context('./presentation.mplstyle'):
+            self.ax.clear()
+            self.ax.grid(True)
+            self.fig.canvas.draw()
+        return self
+
+    def _plot_of_ephemeris(self, event):
+        title = ''
+        with mpl.style.context('./presentation.mplstyle'):
+            if self._periodType == 0:
+                self.ax.plot(calcShort_ephem(self.catalog, self.ephemerisBox.get()))
+                title = 'Короткие интервалы'
+            else:
+                self.ax.plot(calcLong_ephem(self.catalog,
+                    self.ephemerisBox.get(), self._scalePeriod.get()))
+                title = 'Длинные интервалы'
+            self.ax.set_xlabel(r'Epoch')
+            self.ax.set_ylabel(r'%s' % self.ephemerisBox.get())
+            self.ax.set_title(title)
+            self.ax.grid(True, color='white', linewidth=2, alpha=0.3)
+            self.fig.canvas.draw()
+        return self
+
